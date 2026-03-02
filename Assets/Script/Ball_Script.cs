32b659d3f4cbcf2ab4e74802a3304ef8255910ce
Yanno <128984732+Im-Yannooo@users.noreply.github.com>
2026-03-02 22:59:22 +0800

    Reset Ball Function Added with Compare tag for zone

--- Assets/Script/Ball_Script.cs
+++ Assets/Script/Ball_Script.cs
@@ -5,7 +5,8 @@ public class Ball_Script : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float initialAngle = 0.67f;
+    [SerializeField] private float initialAngle = 1.0f;
+    [SerializeField] private float angleY;
     void Start()
     {
         BallMovement();
@@ -14,19 +15,26 @@ public class Ball_Script : MonoBehaviour
     private void BallMovement()
     {
         Vector2 dir = Random.value < 0.5f ? Vector2.left : Vector2.right;
-
-        dir.y  = Random.Range(-initialAngle, initialAngle);
-        rb.linearVelocity = Vector2.left * speed;
+        dir.y = Random.Range(-initialAngle, initialAngle);
+        rb.linearVelocity = dir * speed;
     }
 
     private void ResetBall()
     {
-        //float posY = Random.Range();
+        float positionY = Random.Range(-angleY, angleY);
+        Vector2 startPos = new Vector2(0f,positionY);
+        transform.position = startPos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("ScoreZone"))
+        {
+            ResetBall();
+            BallMovement();
+        }
     }
+
+
 
 }
--- Assets/Script/Score_Zone_Script.cs
+++ Assets/Script/Score_Zone_Script.cs
@@ -4,6 +4,9 @@ public class Score_Zone_Script : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("YAY");
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("SCORE");
+        }
     }
 }
