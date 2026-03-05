adf2319b5244c9ad0c138d6dfbd02217f9f8ec85
Yanno <128984732+Im-Yannooo@users.noreply.github.com>
2026-03-05 23:31:53 +0800

    Game Manager & Scripts
    In this commit, i have added a game manager and additional scripts for my pong games functions and the movement works now YAY!!!

--- Assets/Script/Score_Zone_Script.cs
+++ Assets/Script/Score_Zone_Script.cs
@@ -1,12 +1,15 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class Score_Zone_Script : MonoBehaviour
 {
+    [SerializeField] private TextMeshPro text;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            Debug.Log("SCORE");
+            
         }
     }
 }
